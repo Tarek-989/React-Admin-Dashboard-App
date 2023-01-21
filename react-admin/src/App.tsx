11a8a22a7b46a34108b862e952b89584f2da0d90
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-01-21 14:49:47 +0200

    build Contacts Container

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -5,6 +5,7 @@ import { SideBar, TopBar } from "./pages/global";
 import { Route, Routes } from "react-router-dom";
 import { DashboardContainer } from "./pages/dashboard/DashboardContainer";
 import { TeamContainer } from "./pages/team/TeamContainer";
+import { ContactsContainer } from "./pages/contacts/ContactsContainer";
 
 export const App: React.FC = () => {
 	const [theme, colorMode] = useMode();
@@ -18,9 +19,9 @@ export const App: React.FC = () => {
 						<TopBar />
 						<Routes>
 							<Route path="/" element={<DashboardContainer />} />
-							{/* <Route path="/contacts" element={<ContactsContainer />} /> */}
 							{/* <Route path="/invoices" element={<InvoicesContainer />} /> */}
 							<Route path="/team" element={<TeamContainer />} />
+							<Route path="/contacts" element={<ContactsContainer />} />
 							{/* <Route path="/form" element={<FormContainer />} /> */}
 							{/* <Route path="/bar" element={<BarContainer />} /> */}
 							{/* <Route path="/pie" element={<PieContainer />} /> */}
---
+++ react-admin/src/pages/contacts/ContactsContainer.tsx
@@ -0,0 +1,101 @@
+import React, { useMemo } from 'react';
+import { Box, useTheme } from '@mui/material';
+import { DataGrid, GridColDef, GridColumns, GridToolbar } from '@mui/x-data-grid';
+import { tokens } from '../../theme';
+import { mockDataContacts } from '../../data/mockData';
+import { Header } from '../../components';
+
+export const ContactsContainer: React.FC = () => {
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+	const columns: GridColDef[] = useMemo((): GridColumns => [
+		{
+			field: 'id',
+			headerName: 'ID',
+			flex: 0.5,
+		},
+		{
+			field: 'registrarId',
+			headerName: 'Registrar ID',
+		},
+		{
+			field: 'name',
+			headerName: 'Name',
+			flex: 1,
+			cellClassName: 'name-column--cell',
+		},
+		{
+			field: 'age',
+			headerName: 'Age',
+			type: 'number',
+			headerAlign: 'left',
+			align: 'left'
+		},
+		{
+			field: 'phone',
+			headerName: 'Phone',
+			flex: 1,
+		},
+		{
+			field: 'email',
+			headerName: 'Email',
+			flex: 1,
+		},
+		{
+			field: 'address',
+			headerName: 'Address',
+			flex: 1,
+		},
+		{
+			field: 'city',
+			headerName: 'City',
+			flex: 1,
+		},
+		{
+			field: 'zipCode',
+			headerName: 'Zip Code',
+			flex: 1,
+		},
+	], []);
+	return (
+		<Box m={1.25}>
+			<Header title='CONTACTS' subTilte='List of Contacts for Future Reference' />
+			<Box
+				mt={2.5}
+				height={'75vh'}
+				sx={{
+					'& .MuiDataGrid-root': {
+						border: 'none'
+					},
+					'& .MuiDataGrid-cell': {
+						borderBottom: 'none'
+					},
+					'& .name-column--cell': {
+						color: colors.greenAccent[300]
+					},
+					'& .MuiDataGrid-columnHeaders': {
+						backgroundColor: colors.blueAccent[700],
+						borderBottom: 'none'
+					},
+					'& .MuiDataGrid-virtualScroller': {
+						backgroundColor: colors.primary[400],
+					},
+					'& .MuiDataGrid-footerContainer': {
+						borderTop: 'none',
+						backgroundColor: colors.blueAccent[700],
+					},
+					'& .MuiDataGrid-toolbarContainer .MuiButton-text': {
+						color: `${colors.gray[100]} !important`,
+					},
+
+				}}
+			>
+				<DataGrid
+					rows={mockDataContacts}
+					columns={columns}
+					components={{ Toolbar: GridToolbar }}
+				/>
+			</Box>
+		</Box>
+	);
+};
