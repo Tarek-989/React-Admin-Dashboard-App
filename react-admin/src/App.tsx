c212f5e9299f199ab368f6b8174782e683588b8b
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-01-21 09:28:30 +0200

    build topBar

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, Theme, ThemeProvider } from "@mui/material";
+import { TopBar } from "./pages/global";
 
-const App = () => {
+export const App: React.FC = () => {
 	const [theme, colorMode] = useMode();
 	return (
-		<ColorModeContext.Provider value={colorMode as {toggleColorMode: () => void;}}>
+		<ColorModeContext.Provider value={colorMode as { toggleColorMode: () => void; }}>
 			<ThemeProvider theme={theme as Theme}>
 				<CssBaseline />
 				<div className="app">
-					<main className="content"></main>
+					<main className="content">
+						<TopBar />
+					</main>
 				</div>
 			</ThemeProvider>
 		</ColorModeContext.Provider>
 	);
 }
 
-export default App;
--- react-admin/src/pages/global/TopBar.tsx
+++ react-admin/src/pages/global/TopBar.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import {
+	Search,
+	Notifications,
+	LightModeOutlined,
+	SettingsOutlined,
+	PersonOutline,
+	DarkModeOutlined,
+} from '@mui/icons-material';
+import { Box, IconButton, InputBase, useTheme } from '@mui/material';
+import React, { useContext } from 'react';
+import { ColorModeContext, tokens } from '../../theme';
 
 export const TopBar: React.FC = () => {
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+	const colorMode = useContext(ColorModeContext)
 	return (
-		<>
-			TopBar
-		</>
+		<Box
+			display={'flex'}
+			justifyContent={'space-between'}
+			p={2}
+		>
+			{/* Search bar */}
+			<Box
+				display={'flex'}
+				borderRadius={'0.188rem'}
+				bgcolor={colors.primary[400]}
+			>
+				<InputBase
+					sx={{
+						ml: 2, flex: 1
+					}}
+					placeholder={'Search'}
+				/>
+				<IconButton type='button' sx={{ p: 1 }}>
+					<Search />
+				</IconButton>
+			</Box>
+
+			{/* Icons */}
+			<Box display={'flex'}>
+				<IconButton onClick={colorMode.toggleColorMode}>
+					{theme.palette.mode === 'dark' ? <DarkModeOutlined /> : <LightModeOutlined />}
+				</IconButton>
+				<IconButton>
+					<Notifications />
+				</IconButton>
+				<IconButton>
+					<SettingsOutlined />
+				</IconButton>
+				<IconButton>
+					<PersonOutline />
+				</IconButton>
+			</Box>
+		</Box>
 	);
 };
