28ceeda43c19bc2ada50c0b517f9025a2230877e
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-01-21 10:37:28 +0200

    build Routes

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, Theme, ThemeProvider } from "@mui/material";
-import { TopBar } from "./pages/global";
+import { SideBar, TopBar } from "./pages/global";
+import { Route, Routes } from "react-router-dom";
+import { DashboardContainer } from "./pages/dashboard/DashboardContainer";
 
 export const App: React.FC = () => {
 	const [theme, colorMode] = useMode();
@@ -10,8 +12,22 @@ export const App: React.FC = () => {
 			<ThemeProvider theme={theme as Theme}>
 				<CssBaseline />
 				<div className="app">
+					<SideBar />
 					<main className="content">
 						<TopBar />
+						<Routes>
+							<Route path="/" element={<DashboardContainer />} />
+							{/* <Route path="/team" element={<DashboardContainer />} /> */}
+							{/* <Route path="/contacts" element={<DashboardContainer />} /> */}
+							{/* <Route path="/invoices" element={<DashboardContainer />} /> */}
+							{/* <Route path="/form" element={<DashboardContainer />} /> */}
+							{/* <Route path="/bar" element={<DashboardContainer />} /> */}
+							{/* <Route path="/pie" element={<DashboardContainer />} /> */}
+							{/* <Route path="/line" element={<DashboardContainer />} /> */}
+							{/* <Route path="/faq" element={<DashboardContainer />} /> */}
+							{/* <Route path="/geography" element={<DashboardContainer />} /> */}
+							{/* <Route path="/calender" element={<DashboardContainer />} /> */}
+						</Routes>
 					</main>
 				</div>
 			</ThemeProvider>
