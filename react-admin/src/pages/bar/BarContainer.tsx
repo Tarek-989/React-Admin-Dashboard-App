ff7c39e8245fd46ed806e290e6f7c0fc6cfc313f
Tarek Ahmad <73943097+Tarek-989@users.noreply.github.com>
2023-01-23 08:56:12 +0200

    Revert "hide bar chart because it's not working on React 18.2.0"
    This reverts commit 5a3cdbdab634521df4642e3dbe7627436251c26e.

--- react-admin/src/pages/bar/BarContainer.tsx
+++ react-admin/src/pages/bar/BarContainer.tsx
@@ -6,7 +6,7 @@ import { BarChart, Header } from '../../components';
 export const BarContainer: React.FC = () => {
 	return (
 		<Box m={1.25}>
-			<Header title='BAR CHART' subTilte='Simple Bar Chart' />
+			<Header title='BAR CHART' subTilte='Simple Bar Chart ' />
 			<Box height={'75vh'}>
 				<BarChart isDashboard={false} />
 			</Box>
